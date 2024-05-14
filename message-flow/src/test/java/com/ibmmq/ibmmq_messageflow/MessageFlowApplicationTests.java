e7b3e673a8e60f2abeba8235881a4d1b00062331
JayCesar <batistajulio215@gmail.com>
2024-05-14 14:18:44 -0300

    @JayCesar | feat: adds spring framework

---
+++ message-flow/src/test/java/com/ibmmq/ibmmq_messageflow/MessageFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibmmq.ibmmq_messageflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
