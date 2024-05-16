4da070338498885a2b9f9cf5b6fcf2cb9f9498c2
JayCesar <batistajulio215@gmail.com>
2024-05-15 22:18:38 -0300

    @JayCesar | Feat: add AccounterController and AccountRequester file

--- MessageFlow/src/main/java/com/ibmmq/messageflow/controller/AccountController.java
+++ MessageFlow/src/main/java/com/ibmmq/messageflow/controller/AccountController.java
@@ -8,12 +8,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class AccountController implements AccountRepository {
+public class AccountController {
 
     private List<Account> accountList = new ArrayList<>();
     int number;
 
-    @Override
     public void findByNumber(int number) {
         var account = findInCollection(number);
 
@@ -23,79 +22,79 @@ public class AccountController implements AccountRepository {
             System.out.println("\nA Conta número: " + number + " não foi encontrada!");
     }
 
-    @Override
-    public void listAll() {
-        for (var account : accountList) {
-            account.visualize();
-        }
-    }
-
-    @Override
-    public void create(Account account) {
-        accountList.add(account);
-        System.out.println("\nA Conta número: " + account.getNumber() + " foi criada com sucesso!");
-    }
-
-    @Override
-    public void update(Account account) {
-        var buscaConta = findInCollection(account.getNumber());
-
-        if (buscaConta != null) {
-            accountList.set(accountList.indexOf(buscaConta), account);
-            System.out.println("\nA Conta numero: " + account.getNumber() + " foi atualizada com sucesso!");
-        } else
-            System.out.println("\nA Conta numero: " + account.getNumber() + " não foi encontrada!");
-    }
-
-    @Override
-    public void delete(int number) {
-        var conta = findInCollection(number);
-
-        if (conta != null) {
-            if (accountList.remove(conta) == true)
-                System.out.println("\nA Conta numero: " + number + " foi deletada com sucesso!");
-        } else
-            System.out.println("\nA Conta numero: " + number + " não foi encontrada!");
-    }
-
-    @Override
-    public void withdraw(int number, double amount) {
-        var account = findInCollection(number);
-
-        if (account != null) {
-
-            if (account.withdraw(amount) == true)
-                System.out.println("\nO Saque na Conta numero: " + number + " foi efetuado com sucesso!");
-
-        } else
-            System.out.println("\nA Conta numero: " + number + " não foi encontrada!");
-    }
-
-    @Override
-    public void deposit(int number, double amount) {
-        var account = findInCollection(number);
-
-        if (account != null) {
-            account.deposit(amount);
-            System.out.println("\nO Depósito na Conta numero: " + number + " foi efetuado com sucesso!");
-        } else
-            System.out.println("\nA Conta numero: " + number + " não foi encontrada ou a Conta destino não é uma Conta Corrente!");
-    }
-
-    @Override
-    public void transfer(int sourceNumber, int destinationNumber, double amount) {
-        var sourceAccount = findInCollection(sourceNumber);
-        var targetAccount = findInCollection(destinationNumber);
-
-        if (sourceAccount != null && targetAccount != null) {
-
-            if (sourceAccount.withdraw(amount) == true) {
-                targetAccount.deposit(amount);
-                System.out.println("\nA Transferência foi efetuado com sucesso!");
-            }
-        } else
-            System.out.println("\nA Conta de Origem e/ou Destino não foram encontradas!");
-    }
+//    @Override
+//    public void listAll() {
+//        for (var account : accountList) {
+//            account.visualize();
+//        }
+//    }
+//
+//    @Override
+//    public void create(Account account) {
+//        accountList.add(account);
+//        System.out.println("\nA Conta número: " + account.getNumber() + " foi criada com sucesso!");
+//    }
+//
+//    @Override
+//    public void update(Account account) {
+//        var buscaConta = findInCollection(account.getNumber());
+//
+//        if (buscaConta != null) {
+//            accountList.set(accountList.indexOf(buscaConta), account);
+//            System.out.println("\nA Conta numero: " + account.getNumber() + " foi atualizada com sucesso!");
+//        } else
+//            System.out.println("\nA Conta numero: " + account.getNumber() + " não foi encontrada!");
+//    }
+//
+//    @Override
+//    public void delete(int number) {
+//        var conta = findInCollection(number);
+//
+//        if (conta != null) {
+//            if (accountList.remove(conta) == true)
+//                System.out.println("\nA Conta numero: " + number + " foi deletada com sucesso!");
+//        } else
+//            System.out.println("\nA Conta numero: " + number + " não foi encontrada!");
+//    }
+//
+//    @Override
+//    public void withdraw(int number, double amount) {
+//        var account = findInCollection(number);
+//
+//        if (account != null) {
+//
+//            if (account.withdraw(amount) == true)
+//                System.out.println("\nO Saque na Conta numero: " + number + " foi efetuado com sucesso!");
+//
+//        } else
+//            System.out.println("\nA Conta numero: " + number + " não foi encontrada!");
+//    }
+//
+//    @Override
+//    public void deposit(int number, double amount) {
+//        var account = findInCollection(number);
+//
+//        if (account != null) {
+//            account.deposit(amount);
+//            System.out.println("\nO Depósito na Conta numero: " + number + " foi efetuado com sucesso!");
+//        } else
+//            System.out.println("\nA Conta numero: " + number + " não foi encontrada ou a Conta destino não é uma Conta Corrente!");
+//    }
+//
+//    @Override
+//    public void transfer(int sourceNumber, int destinationNumber, double amount) {
+//        var sourceAccount = findInCollection(sourceNumber);
+//        var targetAccount = findInCollection(destinationNumber);
+//
+//        if (sourceAccount != null && targetAccount != null) {
+//
+//            if (sourceAccount.withdraw(amount) == true) {
+//                targetAccount.deposit(amount);
+//                System.out.println("\nA Transferência foi efetuado com sucesso!");
+//            }
+//        } else
+//            System.out.println("\nA Conta de Origem e/ou Destino não foram encontradas!");
+//    }
 
     // class methods
     public Account findInCollection(int number) {
@@ -105,8 +104,8 @@ public class AccountController implements AccountRepository {
         return null;
     }
 
-    public int generateNumber() {
-        return ++number;
-    }
+//    public int generateNumber() {
+//        return ++number;
+//    }
 
 }
--- MessageFlow/src/main/java/com/ibmmq/messageflow/service/AccountRequester.java
+++ MessageFlow/src/main/java/com/ibmmq/messageflow/service/AccountRequester.java
@@ -1,4 +1,35 @@
 package com.ibmmq.messageflow.service;
 
+import com.ibmmq.messageflow.controller.AccountController;
+import com.ibmmq.messageflow.model.CheckingAccount;
+import com.ibmmq.messageflow.model.SavingsAccount;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
 public class AccountRequester {
+
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(AccountRequester.class, args);
+    }
+
+    public static void createAccounts() {
+        CheckingAccount ca1 = new CheckingAccount(1, 123, 1, "João da Silva", 1000f, 100.0);
+
+//        CheckingAccount ca2 = new CheckingAccount(accounts.generateNumber(), 124, 1, "Maria da Silva", 2000f, 100.0);
+//        accounts.create(ca2);
+//
+//        SavingsAccount sc1 = new SavingsAccount(accounts.generateNumber(), 125, 2, "Mariana dos Santos", 4000f, 12);
+//        accounts.create(sc1);
+//
+//        SavingsAccount sc2 = new SavingsAccount(accounts.generateNumber(), 125, 2, "Juliana Ramos", 8000f, 15);
+//        accounts.create(sc2);
+    }
+
 }
+
+
+
