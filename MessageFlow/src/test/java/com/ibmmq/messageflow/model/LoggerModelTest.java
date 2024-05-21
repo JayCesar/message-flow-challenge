6587b63171b2f1789c4b9f36c196d8ce490a65e9
JayCesar <batistajulio215@gmail.com>
2024-05-20 23:10:16 -0300

    @JayCesar | feat: Application Containerization

--- MessageFlow/src/test/java/com/ibmmq/messageflow/model/LoggerModelTest.java
+++ MessageFlow/src/test/java/com/ibmmq/messageflow/model/LoggerModelTest.java
@@ -14,7 +14,7 @@ public class LoggerModelTest {
         LocalDateTime now = LocalDateTime.now();
         LoggerModel logger = new LoggerModel(Level.INFO, "Test message", now);
 
-        String expectedLogMessage = String.format("LoggerModel{date='%s, level='%s', message='Test message'}'\n",
+        String expectedLogMessage = String.format("LoggerModel{date='%s', level='%s', message='Test message'}'\n",
                 now.format(logger.formatter), Level.INFO);
 
         assertEquals(expectedLogMessage, logger.toString());
